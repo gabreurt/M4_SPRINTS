52c065f57f308c3a2c56505662cc3c016b6aa749
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-11-05 14:36:31 -0300

    Adição de comentários e video do WOKWI

--- SEMANA_03/EXERCICIOS/Programação de microcontroladores/sketch_nov5a/sketch_nov5a.ino
+++ SEMANA_03/EXERCICIOS/Programação de microcontroladores/sketch_nov5a/sketch_nov5a.ino
@@ -1,21 +1,24 @@
+// Professor, fiz o exercício com a ajuda do meu colega de quarto e turma Antonio Ribeiro.
+// Além disso, gostaria de dizer que, meu código que funcionou no WOKWI não performou da mesma forma na IDE, por isso tive que fazer algumas mudanças.
+
 #define led_vermelho 9
 #define led_branco 8
 #define led_azul 17
-#define led_branco 15
+#define led_amarelo 15
 #define botao_salvar 20
 #define botao_ler 21
-#define ldr 14
+#define ldr 38
 #define buzzer 4
 
 int binario[4];
-int armazenado[100];
+int frequencias[100];
 int posicao = 0;
 
 void setup() {
 
-  Serial.begin(115200);
+  // Serial.begin(115200);
   pinMode(led_vermelho, OUTPUT);
-  pinMode(led_branco, OUTPUT);
+  pinMode(led_amarelo, OUTPUT);
   pinMode(led_azul, OUTPUT);
   pinMode(led_branco, OUTPUT);
   pinMode(ldr, INPUT);
@@ -26,7 +29,7 @@ void setup() {
 }
 
 void led_aceso(){
-// nessa funcao, dependendo do numero do binario, determinado led acende ou nao
+// Nesta função, dependendo do numero do binario, determinado led acende ou não.
   if (binario[0] == 0) {
       digitalWrite(led_vermelho, LOW);
     } else {
@@ -46,14 +49,14 @@ void led_aceso(){
     }
 
     if (binario[3] == 0) {
-      digitalWrite(led_branco, LOW);
+      digitalWrite(led_amarelo, LOW);
     } else {
-      digitalWrite(led_branco, HIGH);
+      digitalWrite(led_amarelo, HIGH);
     }
 }
 
 void dec_para_binario(int valor) {
-
+ // transformo decimais para binário usando a estratégia de dividir por 2 e pegar o resto
   for (int i = 0; i <= 3; i++) {
 
     if (valor >= 1) {
@@ -68,40 +71,63 @@ void dec_para_binario(int valor) {
     }
   }
 }
+// int freq_buzzer;
 
 void loop() {
+  // Estes códigos comentados são testes que eu fiz para verificar se todos os meus itens estavam funcionando.
 
+  /* Serial.println(analogRead(ldr));
+  // if (digitalRead(botao_salvar) == LOW){
+  //   digitalWrite(led_azul, !digitalRead(led_azul));
+  //   freq_buzzer = analogRead(ldr);
+  //   tone(buzzer, freq_buzzer, 100);
+  //   delay(100); // debounce (impede que o botão )
+  //   while(digitalRead(botao_salvar) == LOW){}
+  // }
+
+  // if (digitalRead(botao_salvar == LOW)){
+  //   digitalWrite(led_azul, LOW);
+  //   digitalWrite(led_vermelho, LOW);
+  //   digitalWrite(led_branco, LOW);
+  //   digitalWrite(led_amarelo, LOW);
+  //   delay(500);
+  // }  
+  //   if (digitalRead(botao_salvar == HIGH)){
+  //   digitalWrite(led_azul, LOW);
+  //   digitalWrite(led_vermelho, LOW);
+  //   digitalWrite(led_branco, LOW);
+  //   digitalWrite(led_amarelo, LOW);
+  //   delay(500);
+  */   
+
+  // aqui, coloco o valor do ldr numa var
   int valor_LDR = analogRead(ldr);
-
+// divido por 257, que é o valor para leitura do LDR da placa
   int valor_convertido = valor_LDR / 257;
-
-  // 257 é o valor encontrado para leitura do LDR
-  // especificamente na placa ESP32 usada nesse simulador
-
+  // transformo o decimal em binario
   dec_para_binario(valor_convertido);
 
+// aqui coloco para quando o led acender tocar a musica, e quando o botao de ler for clicado, ele tocar todas as frequencias.
   led_aceso();
 
    if (digitalRead(botao_salvar) == LOW) {
 
-    armazenado[posicao] = valor_convertido;
-    tone(buzzer, armazenado[posicao]*100, 350);
-    
+    frequencias[posicao] = valor_convertido;
+    tone(buzzer, frequencias[posicao]*100, 350); 
     posicao += 1;
     delay(50);
-
     while(digitalRead(botao_salvar)== LOW){}
   }
 
   if (digitalRead(botao_ler) == LOW) {
 
     while (posicao >= 0) {
-      tone(buzzer, armazenado[posicao]*100, 350);
-      dec_para_binario(armazenado[posicao]);
+      tone(buzzer, frequencias[posicao]*100, 350);
+      dec_para_binario(frequencias[posicao]);
       delay(500);
       led_aceso();
 
-      Serial.println(armazenado[posicao]*100);
+      Serial.println(frequencias[posicao]*100);
       Serial.println(valor_convertido);
     
       posicao = posicao-1;
